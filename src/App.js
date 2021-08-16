6bc25cd49f6b90f6202d3509d7e8b66aa69481a9
Alexandre Menezes <amenezes031@gmail.com>
2021-08-16 17:27:34 -0300

    add ResetCSS in App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import { Layout } from "./components/Layout";
 import { Profile } from "./components/Profile";
 
+import { ResetCSS } from "./styles/resetCSS";
+
 function App() {
   return (
-    <main>
-      <Layout>
-        <Profile />
+    <>
+      <main>
+        <Layout>
+          <Profile />
 
-        <div>Repositories</div>
+          <div>Repositories</div>
 
-        <div>Starred</div>
-      </Layout>
-    </main>
+          <div>Starred</div>
+        </Layout>
+      </main>
+
+      <ResetCSS />
+    </>
   );
 }
 
