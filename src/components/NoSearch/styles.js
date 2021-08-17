34e818abc4d67c275a790024bdca5023945165bd
Alexandre Menezes <amenezes031@gmail.com>
2021-08-17 19:47:56 -0300

    create NoSearch styled

---
+++ src/components/NoSearch/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const WrapperNoSearch = styled.div`
+  margin: 1rem;
+  display: flex;
+  justify-content: center;
+
+  h1 {
+    font-size: 2rem;
+  }
+`;
