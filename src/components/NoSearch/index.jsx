c9edacd66f63615073cbdf9d66de0fe5b5e4926f
Alexandre Menezes <amenezes031@gmail.com>
2021-08-17 19:48:05 -0300

    create NoSearch component

---
+++ src/components/NoSearch/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import * as S from "./styles";
+
+export const NoSearch = () => {
+  return (
+    <S.WrapperNoSearch>
+      <h1>Nenhum usuário pesquisado</h1>
+    </S.WrapperNoSearch>
+  );
+};
