3e7ddee435e3a55ec5e88e8d7e6cf58a51e6c943
Alexandre Menezes <amenezes031@gmail.com>
2021-08-17 22:59:41 -0300

    create get repo and get starred functions

--- src/hooks/GithubHooks.js
+++ src/hooks/GithubHooks.js
@@ -2,9 +2,12 @@ import { useContext } from "react";
 import { GithubContext } from "../providers/GithubProvider";
 
 export const useGithub = () => {
-  const { githubState, getUser } = useContext(GithubContext);
+  const { githubState, getUser, getUserRepos, getUserStarred } =
+    useContext(GithubContext);
   return {
     githubState,
     getUser,
+    getUserRepos,
+    getUserStarred,
   };
 };
